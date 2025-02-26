2af416ed3ca7ce5e40741c97196cc224a0799818
Mathias Rørvik <1775685+cyanboy@users.noreply.github.com>
2025-02-26 11:27:44 +0100

    tweak fulltext searchj

--- migrations/0003_add_full_text_search.sql
+++ migrations/0003_add_full_text_search.sql
@@ -1,9 +1,7 @@
 CREATE VIRTUAL
 TABLE repositories_fts USING fts5 (
-    name,
-    namespace,
-    description,
-    languages
+    name, namespace, description, languages,
+    tokenize = 'unicode61 remove_diacritics 2'
 );
 
 INSERT INTO
@@ -14,102 +12,74 @@ INSERT INTO
         description,
         languages
     )
-SELECT r.id, r.name, r.namespace, r.description, COALESCE(
+SELECT r.id, r.name, r.namespace, r.description, IFNULL(
         (
-            SELECT GROUP_CONCAT(l.name, ' ')
+            SELECT GROUP_CONCAT(
+                    l.name, ' '
+                    ORDER BY l.name
+                )
             FROM
                 repository_languages rl
                 JOIN languages l ON rl.language_id = l.id
             WHERE
                 rl.repository_id = r.id
         ), ''
-    ) AS languages
+    )
 FROM repositories r;
 
-CREATE TRIGGER repositories_fts_insert
-        AFTER INSERT ON repositories
-        BEGIN
-            INSERT INTO repositories_fts (rowid, name, namespace, description, languages)
-            VALUES (
-                NEW.id,
-                NEW.name,
-                NEW.namespace,
-                NEW.description,
-                COALESCE((
-                    SELECT GROUP_CONCAT(l.name, ' ')
-                    FROM repository_languages rl
-                    JOIN languages l ON rl.language_id = l.id
-                    WHERE rl.repository_id = NEW.id
-                ), '')
-            );
-END;
-
-CREATE TRIGGER repositories_fts_update AFTER
-UPDATE ON repositories BEGIN
-DELETE FROM repositories_fts
-WHERE
-    rowid = OLD.id;
-
-INSERT INTO
-    repositories_fts (
-        rowid,
-        name,
-        namespace,
-        description,
-        languages
-    )
-VALUES (
+CREATE TRIGGER repositories_fts_insert AFTER INSERT ON repositories BEGIN
+    INSERT INTO repositories_fts (rowid, name, namespace, description, languages)
+    VALUES (
         NEW.id,
         NEW.name,
         NEW.namespace,
         NEW.description,
-        COALESCE(
-            (
-                SELECT GROUP_CONCAT(l.name, ' ')
-                FROM
-                    repository_languages rl
-                    JOIN languages l ON rl.language_id = l.id
-                WHERE
-                    rl.repository_id = NEW.id
-            ),
-            ''
-        )
+        IFNULL((
+            SELECT GROUP_CONCAT(l.name, ' ' ORDER BY l.name)
+            FROM repository_languages rl
+            JOIN languages l ON rl.language_id = l.id
+            WHERE rl.repository_id = NEW.id
+        ), '')
     );
 END;
 
-CREATE TRIGGER repositories_fts_delete AFTER DELETE ON repositories BEGIN
-DELETE FROM repositories_fts
-WHERE
-    rowid = OLD.id;
+CREATE TRIGGER repositories_fts_update AFTER UPDATE ON repositories BEGIN
+    UPDATE repositories_fts
+    SET
+        name = NEW.name,
+        namespace = NEW.namespace,
+        description = NEW.description,
+        languages = IFNULL((
+            SELECT GROUP_CONCAT(l.name, ' ' ORDER BY l.name)
+            FROM repository_languages rl
+            JOIN languages l ON rl.language_id = l.id
+            WHERE rl.repository_id = NEW.id
+        ), '')
+    WHERE rowid = NEW.id;
 END;
 
-CREATE TRIGGER repository_languages_insert
-        AFTER INSERT ON repository_languages
-        BEGIN
-            UPDATE repositories_fts
-            SET languages = COALESCE((
-                SELECT GROUP_CONCAT(l.name, ' ')
-                FROM repository_languages rl
-                JOIN languages l ON rl.language_id = l.id
-                WHERE rl.repository_id = NEW.repository_id
-            ), '')
-            WHERE rowid = NEW.repository_id;
-        END;
+CREATE TRIGGER repositories_fts_delete AFTER DELETE ON repositories BEGIN
+    DELETE FROM repositories_fts WHERE rowid = OLD.id;
+END;
+
+CREATE TRIGGER repository_languages_insert AFTER INSERT ON repository_languages BEGIN
+    UPDATE repositories_fts
+    SET languages = IFNULL((
+        SELECT GROUP_CONCAT(l.name, ' ' ORDER BY l.name)
+        FROM repository_languages rl
+        JOIN languages l ON rl.language_id = l.id
+        WHERE rl.repository_id = NEW.repository_id
+    ), '')
+    WHERE rowid = NEW.repository_id;
+END;
 
 CREATE TRIGGER repository_languages_delete AFTER DELETE ON repository_languages BEGIN
-UPDATE repositories_fts
-SET
-    languages = COALESCE(
-        (
-            SELECT GROUP_CONCAT(l.name, ' ')
-            FROM
-                repository_languages rl
-                JOIN languages l ON rl.language_id = l.id
-            WHERE
-                rl.repository_id = OLD.repository_id
-        ),
-        ''
-    )
-WHERE
-    rowid = OLD.repository_id;
+    UPDATE repositories_fts
+    SET languages = IFNULL((
+        SELECT GROUP_CONCAT(l.name, ' ' ORDER BY l.name)
+        FROM repository_languages rl
+        JOIN languages l ON rl.language_id = l.id
+        WHERE rl.repository_id = OLD.repository_id
+    ), '')
+    WHERE rowid = OLD.repository_id;
 END;
